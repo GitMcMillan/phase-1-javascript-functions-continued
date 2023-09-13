6682f0b173e2741b2e65d22139a12f672d56fa1e
Alexander <mcmillancontact@protonmail.com>
2023-09-13 13:01:32 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+      return `This Saturday, I want to ${activity}!`;
+  }
+
+  saturdayFun();
+  saturdayFun("bathe my dog!")
+
+
+
+  function mondayWork(activity = "go to the office") {
+      return `This Monday, I will ${activity}.`
+  }
+
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"))
+
+
+  function wrapAdjective(flair = "*") {
+      return function innerFunction(spec = "special") {
+            return `You are ${flair}${spec}${flair}!`
+      }
+  }
+
+console.log(wrapAdjective("headache")("painful"))
