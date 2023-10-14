17722c51f7fc160da3e873669fd2ce6fd02749dd
alexandermcmillan <mcmillancontact@protonmail.com>
2023-10-13 20:41:14 -0700

    lab completed

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 // code your solution here
+
+//empty parameter = roller-skate
+// argument passed = override roller-skate
 function saturdayFun(activity = "roller-skate") {
-      return `This Saturday, I want to ${activity}!`;
-  }
-
-  saturdayFun();
-  saturdayFun("bathe my dog!")
+    return `This Saturday, I want to ${activity}!`;
+}
 
 
 
-  function mondayWork(activity = "go to the office") {
-      return `This Monday, I will ${activity}.`
-  }
+// function saturdayFun(roller-skate) {
+//     console.log(`This Saturday, I want to ${`roller-skate`}!`)
 
-  console.log(mondayWork());
-  console.log(mondayWork("work from home"))
+// }
+//function saturdayFun("bathe my dog") 
+// "This Saturday, I want to bathe my dog!"
+
+// Implement a function expression called mondayWork. The function should return a String like "This Monday, I will ... ." Fill in the ... with the activity that's passed in as the first parameter. If nothing is passed in, default to "go to the office". Run npm test to verify you've gotten this set of tests passing before continuing with the lesson.
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+};
 
 
-  function wrapAdjective(flair = "*") {
-      return function innerFunction(spec = "special") {
-            return `You are ${flair}${spec}${flair}!`
-      }
-  }
 
-console.log(wrapAdjective("headache")("painful"))
+// function wrapAdjective(flair = "*") {
+//     return function innerFunction(spec = "special") {
+//         return `You are ${flair}${spec}${flair}!`
+//     }
+// }
+
+
+function wrapAdjective(pretty = `*`) {
+    return function youAre(very = special) {
+        return `You are ${pretty}${very}${pretty}!`
+    }
+}
